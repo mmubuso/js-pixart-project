c4fa22a7814d94999bab940fc55de0748ca8d6d9
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-08 21:48:52 -0400

    color is changing when set color button is clicked

--- pixart.js
+++ pixart.js
@@ -1,13 +1,19 @@
-/* ### Commit 1
+/* ### Commit 2
 
 * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
 * You can use `document.querySelector` (or another document method) to select the element, then add an event listener. */
 
 //Variables
 const setColorButton = document.getElementById('set-color')
-const textInput = document.getElementById('color-field')
+const textField = document.getElementById('color-field')
+const brushBox = document.getElementsByClassName('brush')[0]
 
-//Intitalizations
 
+//EventListernes
 
-//Functions
+//Changes color of brush box depending on whats typed in
+setColorButton.addEventListener('click', function(event){
+   brushBox.style.backgroundColor = textField.value
+   event.preventDefault()
+})
+
