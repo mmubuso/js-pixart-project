d4999e92204c33c51c1a491ed4051f79dde60488
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-08 23:31:44 -0400

    Clicking in div w/ square class turns it green

--- pixart.js
+++ pixart.js
@@ -7,11 +7,20 @@ const setColorButton = document.getElementById('set-color')
 const textField = document.getElementById('color-field')
 const brushBox = document.getElementsByClassName('brush')[0]
 
+//Event listerners 
 //Changes color of brush box depending on whats typed in
 setColorButton.addEventListener('click', function(event){
    brushBox.style.backgroundColor = textField.value
    event.preventDefault()
 })
+
+// Change the color of the small div square to green when clicked on
+document.body.addEventListener('click',event => {
+    if(event.target.classList.value === "square"){
+        event.target.style.backgroundColor = 'green'
+    }
+})
+
 
 //Create 20 divs with the class of square
 for(let i = 0; i < 20; i++){
@@ -19,4 +28,5 @@ for(let i = 0; i < 20; i++){
     document.body.appendChild(canvasBlock)
     canvasBlock.classList.add('square')
 }
+
 
