042354b66d52532c1d5af7cf06c36e036aa015a5
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-08 21:23:20 -0400

    captured set color button and text input field in const variables

---
+++ pixart.js
@@ -0,0 +1,13 @@
+/* ### Commit 1
+
+* When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
+* You can use `document.querySelector` (or another document method) to select the element, then add an event listener. */
+
+//Variables
+const setColorButton = document.getElementById('set-color')
+const textInput = document.getElementById('color-field')
+
+//Intitalizations
+
+
+//Functions
