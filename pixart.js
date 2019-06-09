e2b55bd32ef05484b54bd15f658a353c25f70530
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-08 22:52:01 -0400

    added 20 divs with a class of square

--- pixart.js
+++ pixart.js
@@ -1,19 +1,22 @@
-/* ### Commit 2
+//Musiteli Mubuso
+//Pix Art Challenge exercise
 
-* When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
-* You can use `document.querySelector` (or another document method) to select the element, then add an event listener. */
 
 //Variables
 const setColorButton = document.getElementById('set-color')
 const textField = document.getElementById('color-field')
 const brushBox = document.getElementsByClassName('brush')[0]
 
-
-//EventListernes
-
 //Changes color of brush box depending on whats typed in
 setColorButton.addEventListener('click', function(event){
    brushBox.style.backgroundColor = textField.value
    event.preventDefault()
 })
+
+//Create 20 divs with the class of square
+for(let i = 0; i < 20; i++){
+    let canvasBlock = document.createElement('div')
+    document.body.appendChild(canvasBlock)
+    canvasBlock.classList.add('square')
+}
 
